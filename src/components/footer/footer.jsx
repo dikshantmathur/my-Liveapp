fb27a6d50b87c7f2f1c976187a9e853bc44ab988
dikshantmathur <dikshant.17jice038@jietjodhpur.ac.in>
2020-07-21 15:11:45 +0530

    Live React App

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer alert-primary">
+                <div className="container ">
+                    <p className="fofo text-center">React App made by Dikshant Mathur</p>
+                </div>
+            </footer>
+        );
+    }
+}
+
